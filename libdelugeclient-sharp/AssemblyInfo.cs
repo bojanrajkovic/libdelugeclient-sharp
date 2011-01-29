5bb86554e97d9c91b5c65127fd3805e5230b34df
Bojan Rajkovic <bojan@airpoweranalytics.com>
2011-01-29 12:03:38 -0500

    Initial commit of libdelugeclient-sharp.

---
+++ libdelugeclient-sharp/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("libdelugeclient-sharp")]
+[assembly: AssemblyDescription("A JSON-RPC client for Deluge.")]
+[assembly: AssemblyProduct("libdelugeclient-sharp")]
+[assembly: AssemblyCopyright("Bojan Rajkovic © 2011")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("0.1.*")]
+
